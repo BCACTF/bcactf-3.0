27ab1f170c813981a7eefb3af51a5c54305eaba0
BuildTools <unconfigured@null.spigotmc.org>
2022-03-28 21:54:15 -0400

    uploaded

---
+++ js/adminLog.js
@@ -0,0 +1,55 @@
+const unameElem = document.getElementById("uname");
+const pwdElem = document.getElementById("pwd");
+
+document.addEventListener('keyup', (e)=>{
+    switch (e.key) {
+        case "Enter":
+            handleInput();
+            break;
+    }
+});
+
+function handleInput() {
+    let uname=unameElem.value;
+    if (uname != "admin") {
+        alert("You're not admin");
+        return;
+    }
+
+    let pwd = pwdElem.value;
+    let encodedPwd = fancyEncode(pwd);
+
+    setCookie("pwd", encodedPwd, 365);
+
+    window.location.replace("adminEditor.html");
+
+}
+
+function setCookie(name, value, days) {
+    const d = new Date();
+    d.setTime(d.getTime() + (days*24*60*60*1000));
+    let expires = "expires="+ d.toUTCString();
+    document.cookie = name + "=" + value + ";" + expires + ";path=/;samesite=None;secure;";
+}
+function getCookie(cookiename) {
+    let name = cookiename + "=";
+    let decodedCookie = decodeURIComponent(document.cookie);
+    let ca = decodedCookie.split(';');
+    for(let i = 0; i <ca.length; i++) {
+      let c = ca[i];
+      while (c.charAt(0) == ' ') {
+        c = c.substring(1);
+      }
+      if (c.indexOf(name) == 0) {
+        return c.substring(name.length, c.length);
+      }
+    }
+    return "";
+}
+function fancyEncode(str) {
+    let newString = "";
+    for (i=0;i<=str.length-1;i++) {
+        newString += str.charCodeAt(i) + "e";
+    }
+    return newString;
+}
---
+++ js/editor.js
@@ -0,0 +1,3 @@
+if (getCookie("pwd") == "98e99e97e99e116e102e123e117e36e101e114e115e95e115e51e51e95e99e48e48e107e33e101e115e95e55e111e111e125e") {
+    window.location.replace("flag.html");
+}
