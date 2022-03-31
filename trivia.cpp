3cfdc417b26a9e4d62b473ffe893feb5bd411e8e
Jacob Berger <tkdpenguin123@gmail.com>
2022-03-31 09:38:48 -0400

    Trivia works now

--- trivia.cpp
+++ trivia.cpp
@@ -4,31 +4,31 @@ using namespace std;
 
 int main() {
     string question1;
-    cout << "Who am I? \n";
+    cout << "What is my first name? \n";
     cin >> question1;
 
-    if (question1 == "Jacob Berger") {
-        cout << "Correct! Next Question";
+    if (question1 == "Jacob") {
+        cout << "Correct! Next Question: \n";
     }else {
         return 0;
     }
 
     string question2;
-    cout << "What is 1+1*2?";
+    cout << "What is 1+1*2? \n";
     cin >> question2;
 
     if (question2 == "3") {
-        cout << "Correct! Next Question";
+        cout << "Correct! Next Question \n";
     }else {
         return 0;
     }
 
     string question3;
-    cout << "How many letters were in the Latin alphabet used by the Romans during the Classical Latin period";
+    cout << "How many letters were in the Latin alphabet used by the Romans during the Classical Latin period \n";
     cin >> question3;
 
     if (question3 == "23") {
-        cout << "Correct! Next Question";
+        cout << "Correct! Next Question \n";
     }else {
         return 0;
     }
@@ -38,17 +38,17 @@ int main() {
     cin >> question4;
 
     if (question4 == "Ankara") {
-        cout << "Correct! Next Question";
+        cout << "Correct! Next Question \n";
     }else {
         return 0;
     }
 
     string question5;
-    cout << "Who is Hades's wife?";
+    cout << "Who is Hades's wife? \n";
     cin >> question5;
 
     if (question5 == "Persephone") {
-        cout << "Correct! Next Question";
+        cout << "Correct! Next Question \n";
     }else {
         return 0;
     }
@@ -58,7 +58,7 @@ int main() {
     cin >> question6;
 
     if (question6 == "Chiron") {
-        cout << "Correct! Next Question";
+        cout << "Correct! Next Question \n";
     }else {
         return 0;
     }
