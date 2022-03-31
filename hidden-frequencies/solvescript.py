8d151788e1ba17af4991d13979f003a2fb0b8fa5
Yusuf Sallam <68395788+yusufsallam64@users.noreply.github.com>
2022-03-31 19:56:45 -0400

    add hidden-frequencies

---
+++ hidden-frequencies/solvescript.py
@@ -0,0 +1,13 @@
+f = open('hidden-frequencies/msg.txt')
+seen = {}
+  
+for line in f:
+    for character in line:
+        if character in seen:
+            seen[character] += 1
+        else:
+            seen[character] = 1
+
+for key in seen:
+    print(chr(seen.get(key)), end="")
+print()
