a2279595fd3201cbcc5b267ae8949dccd900d9ad
aBacIdEe <maxgong2007@gmail.com>
2022-04-27 16:07:30 -0400

    new problem time

---
+++ Jpegs/PNGtoJPEG.py
@@ -0,0 +1,17 @@
+import PIL
+import numpy as np
+
+# convert image to a matrix
+
+# perform DCT on it
+# use the quantization table on it
+# Read the matrix from top left to bottom right order
+# compress said file
+
+# make a file containing the image and the quantization table
+
+# give the problem solver a library to open the file type
+# uncompress the file
+# undo the quantization
+# use inverse DCT
+# convert back to an image
