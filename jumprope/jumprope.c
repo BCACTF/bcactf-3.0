eafe4ecd2f64ffa9533c58ea4cf417106e13f501
Yusuf Sallam <68395788+yusufsallam64@users.noreply.github.com>
2022-05-10 16:00:11 -0400

    fix stuff b/c git

--- jumprope/jumprope.c
+++ jumprope/jumprope.c
@@ -8,16 +8,20 @@ void a() {
         printf("\nLooks like we've run out of jump ropes...\n");
         printf("Challenge is misconfigured. Please contact admin if you see this.\n");
     }
-    
+
     fgets(flag, sizeof(flag), fptr);
     puts(flag);
 }
 
-int main() {    
-    char arr[500];
+void jumprope(){
+	char arr[500];
+	printf("\nBetter start jumping!\n");
+	gets(arr);
+    printf("Woo, that was quite the workout wasn't it!\n");
+}
+
+int main() {
     printf("Here at BCA, fitness is one of our biggest priorities!\n");
     printf("Today's workout is going to be jumproping. Enjoy!\n");
-    gets(arr);
-    printf("Woo, that was quite the workout wasn't it!\n");
-
+	jumprope();
 }
