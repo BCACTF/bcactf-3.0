8caa10d84581b12d535a486f5c59f590e4d31d5e
glacialcascade <11969863+glacialcascade@users.noreply.github.com>
2022-05-11 17:53:23 -0400

    add my-first-calculator

---
+++ my-first-calculator/index.js
@@ -0,0 +1,22 @@
+function myFirstCalculator(input) {
+    if (input.match(/[a-zA-Z]/)) {
+        return "Bad input. Letters are not allowed!";
+    } else {
+        try {
+            return (eval(input));
+        } catch (e) {
+            return "Calculator returned error: " + e;
+        }
+    }
+}
+
+console.log("Get the flag by making the calculator output \"yoursecond\"!");
+if (process.argv.length < 3) {
+    console.log("Please run this programs with an argument.");
+    process.exit();
+}
+c = myFirstCalculator(process.argv[2]); //might need to change the "< 3" and "[2]" when using netcat
+console.log("Result: " + c);
+if (c === "yoursecond") {
+    console.log("Congrats! The flag is bcactf{G00D_EV4LUAT1ON}");
+}
