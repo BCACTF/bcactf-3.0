6373bfe8bd78326208010e78d00f0f598fa6e2b7
awt-256 <68395886+awt-256@users.noreply.github.com>
2022-05-10 20:11:27 -0400

    reformat

--- jumprope/jumprope.c
+++ jumprope/jumprope.c
@@ -14,14 +14,20 @@ void a() {
 }
 
 void jumprope(){
-	char arr[500];
-	printf("\nBetter start jumping!\n");
-	gets(arr);
+    char arr[500];
+    printf("\nBetter start jumping!\n");
+    gets(arr);
     printf("Woo, that was quite the workout wasn't it!\n");
 }
 
 int main() {
+    setbuf(stdout, NULL);
+    setbuf(stdin, NULL);
+    setbuf(stderr, NULL);
+	
     printf("Here at BCA, fitness is one of our biggest priorities!\n");
     printf("Today's workout is going to be jumproping. Enjoy!\n");
-	jumprope();
+    jumprope();
+
+    return 1;
 }
