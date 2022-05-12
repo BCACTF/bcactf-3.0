b15c90695e015f58af6dd64458b9914a2a05111d
glacialcascade <11969863+glacialcascade@users.noreply.github.com>
2022-05-12 12:43:16 -0400

    rework

--- my-first-calculator/index.js
+++ my-first-calculator/index.js
@@ -1,7 +1,10 @@
 function myFirstCalculator(input) {
     if (input.match(/[a-zA-Z"'\\]/)) {
         return "Bad input. Letters, backslashes, and quote marks are not allowed!";
-    } else {
+    } else if (input.length>45){
+        return "Bad input. Input must be 45 characters long or less"
+    }
+    else {
         try {
             return (eval(input));
         } catch (e) {
@@ -10,13 +13,13 @@ function myFirstCalculator(input) {
     }
 }
 
-console.log("Get the flag by making the calculator output \"yoursecond\"!");
+console.log("Get the flag by making the calculator output \"bcactf\"!");
 if (process.argv.length < 3) {
-    console.log("Please run this programs with an argument.");
+    console.log("Please run this program with an argument.");
     process.exit();
 }
 c = myFirstCalculator(process.argv[2]); //might need to change the "< 3" and "[2]" when using netcat
 console.log("Result: " + c);
-if (c === "yoursecond") {
+if (c === "bcactf") {
     console.log("Congrats! The flag is bcactf{G00D_EV4LUAT1ON}");
 }
