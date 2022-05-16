064d2cf486f2358176e7cae9f6213c56a20e3b41
Yusuf Sallam <68395788+yusufsallam64@users.noreply.github.com>
2022-05-15 21:15:23 -0400

    make printfs have a theme

--- format-fortune/src.c
+++ format-fortune/src.c
@@ -2,18 +2,20 @@
 #include <unistd.h>
 #include <stdlib.h>
 
+int magic = 0;
+
 void printflag() {
     FILE *fptr = fopen("flag.txt", "r");
     char flag[100];
     if(fptr == NULL){
-        puts("Please add flag.txt to the present working directory to test this file.\n");
-        puts("If you see this on the remote server, please contact admin.");
+        puts("Come on... Unfortunately we forgot the flag... get it, unfortunately - ha ha ha.\n");
+        puts("Anyways, challenge is misconfigured. Please contact admin.");
     }
 
     fgets(flag, sizeof(flag), fptr);
     puts(flag);
 }
-int magic = 0;
+
 int main() {
     char buf[50]; 
 
@@ -21,18 +23,19 @@ int main() {
     setbuf(stdin, NULL);
     setbuf(stderr, NULL);
 
-    puts("Hi, what's your name? ");
+    printf("Welcome to my casino! You look like you want to throw your life savings away...\n");
+    printf("Don't tell anyone, but around here, the house ALWAYS wins.\n");
+    printf("Give it a try! But first, what's your name?\n");
     fgets(buf, 50, stdin);
-
-    printf("Welcome to my game of luck, ");
+    printf("Well, good luck, ");
     printf(buf);
     sleep(1);
     puts("Testing your luck... please wait");
     sleep(2);
     if (magic == 0xBEEF) {
-        puts("\nToday's your lucky day! You've won a flag");
+        puts("\nI'm impressed! You've won a flag");
         printflag();
-    } else puts("\nSadly you are not lucky enough... no flag today");
+    } else puts("\I warned you! No flag today.");
 
     return 1;
 }
