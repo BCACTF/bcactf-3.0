68b71af0721e84176613ac70ac55df6aeb74b50a
awt-256 <68395886+awt-256@users.noreply.github.com>
2022-05-18 15:50:14 -0400

    ill test later

--- my-first-calculator/index.js
+++ my-first-calculator/index.js
@@ -1,7 +1,10 @@
+const readline = require('node:readline');
+
 function myFirstCalculator(input) {
-    if (input.match(/[a-zA-Z"'\\]/)) {
+    if (!input) return "Please input text";
+    if (input.match(/[a-zA-Z"`'\/\]/)) {
         return "Bad input. Letters, backslashes, and quote marks are not allowed!";
-    } else if (input.length>45){
+    } else if (input.length > 45){
         return "Bad input. Input must be 45 characters long or less"
     }
     else {
@@ -14,12 +17,16 @@ function myFirstCalculator(input) {
 }
 
 console.log("Get the flag by making the calculator output \"bcactf\"!");
-if (process.argv.length < 3) {
-    console.log("Please run this program with an argument.");
-    process.exit();
-}
-c = myFirstCalculator(process.argv[2]); //might need to change the "< 3" and "[2]" when using netcat
-console.log("Result: " + c);
-if (c === "bcactf") {
+
+const rl = readline.createInterface({ input: process.stdin, output: process.stdin });
+
+rl.question('> ', (response) => {
+  const c = myFirstCalculator(response)
+  console.log("Result: " + c);
+  if (c === "bcactf") {
     console.log("Congrats! The flag is bcactf{G00D_EV4LUAT1ON}");
-}
+  }
+    
+  rl.close();
+  process.exit(0);
+});
