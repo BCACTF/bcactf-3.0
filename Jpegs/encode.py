eaa94b5c87873132f6837510fae82c35203f5592
aBacIdEe <maxgong2007@gmail.com>
2022-05-18 16:31:25 -0400

    more or less done

--- Jpegs/encode.py
+++ Jpegs/encode.py
@@ -1,51 +1,27 @@
 from PIL import Image
 import numpy as np
 import cv2
-import gzip
-np.set_printoptions(suppress=True)
-
-
-# convert image to a matrix
-img = Image.open('test.png')
-# img.show()
+FILE = 'original.png'
+img = Image.open(FILE)
 arr = np.asarray(img, dtype=np.float64)
-
-# convert to grayscale
 grayArr = np.zeros((len(arr), len(arr[0])))
 for vert in range(len(arr)):
     for hori in range(len(arr[0])):
-        grayArr[vert][hori] = arr[vert][hori][0]
-
-# print("After Conversion:")
-# print(grayArr[160:168,160:168])
-
-# crops the image
+        grayArr[vert][hori] = arr[vert][hori]
 nHeight = len(grayArr) - len(grayArr) % 8
 nWidth = len(grayArr[0]) - len(grayArr[0]) % 8
 cropArr = np.zeros((nHeight, nWidth)) # resize dumb
 for i in range(0, nHeight):
     for j in range(0, nWidth):
-        cropArr[i,j] = grayArr[i,j]# - 128
+        cropArr[i,j] = grayArr[i,j] / 2# - 128
 grayArr = cropArr
-# print(grayArr)
-
-# defines DCT
 dctArr = np.zeros((nHeight, nWidth))
-
 def dct2(arr):
     return cv2.dct(arr/255)*255
-    # return fftpack.dct(fftpack.dct(arr, type=2, axis=0, norm='ortho'), type=2, axis=1, norm='ortho')
-
-# uses DCT
 dctArr = np.zeros((nHeight, nWidth))
 for i in range(0, nHeight, 8):
     for j in range(0, nWidth, 8):
         dctArr[i:i+8,j:j+8] = dct2(grayArr[i:i+8,j:j+8])
-
-# print("After DCT:")
-# print(dctArr[160:168,160:168])
-
-# thresholds table
 scalar = np.array([16, 11, 10, 16, 24, 40, 51, 61,
                    12, 12, 14, 19, 26, 58, 60, 55,
                    14, 13, 16, 24, 40, 57, 69, 56,
@@ -55,45 +31,26 @@ scalar = np.array([16, 11, 10, 16, 24, 40, 51, 61,
                    49, 64, 78, 87, 103,121,120,101,
                    72, 92, 95, 98, 112,100,103,99])
 scalar = scalar.reshape((8, 8))
-#scalar = scalar[::-1,::-1].T
-
 reduArr = np.zeros((nHeight, nWidth))
 for i in range(0, nHeight, 8):
     for j in range(0, nWidth, 8):
         block = np.array(dctArr[i:i+8,j:j+8])
         reduArr[i:i+8,j:j+8] = np.divide(block, scalar)
-
 for i in range(0, nHeight):
     for j in range(0, nWidth):
         reduArr[i,j] = reduArr[i,j].round()
         if reduArr[i,j] == 0:
             reduArr[i,j] = 0
-
-# print("After Quantization:")
-# print(reduArr[0:17,0:17])
-# print(reduArr)
-# scales back threshold
-
-# zigzag encoding puts into 1d array for storage
 order = [0, 1, 8, 16, 9, 2, 3, 10, 17, 24, 32, 25, 18, 11, 4, 5, 12, 19, 26, 33, 40, 48, 41, 34, 27, 20, 13, 6, 7, 14, 21, 28, 35, 42, 49, 56, 57, 50, 43, 36, 29, 22, 15, 23, 30, 37, 44, 51, 58, 59, 52, 45, 38, 31, 39, 46, 53, 60, 61, 54, 47, 55, 62, 63]
-
 result = ''
-
-for i in range(0, nHeight, 8): # first dimension
-    for j in range(0, nWidth, 8): # second dimension
-        toBeRead = reduArr[i:i+8, j:j+8].flatten() # converts j first i second wise flattens
+for i in range(0, nHeight, 8):
+    for j in range(0, nWidth, 8):
+        toBeRead = reduArr[i:i+8, j:j+8].flatten()
         for index in order:
             num = toBeRead[index]
-            if num <= 0: sign = '0'
+            if num >= 0: sign = '0'
             else: sign = '1'
-            result += f'{sign}{int(abs(toBeRead[index])):08b}'
-
-text_file = open("temp.txt", "w")
-text_file.write(f'{nHeight:064b}{nWidth:064b}{result}')
-text_file.close()
-
-f_in = open("temp.txt", "rb")
-f_out = gzip.open('image.txt.gz', 'wb')
-f_out.writelines(f_in)
-f_out.close()
-f_in.close()
+            result += f'{sign}{int(abs(toBeRead[index])):07b}'
+compressed_value = f'0{nHeight:07b}0{nWidth:07b}{result}'
+f = open("flag", "wb")
+f.write(int("".join(compressed_value),2).to_bytes(len(compressed_value)//8, byteorder="big"))
--- Jpegs/open.py
+++ Jpegs/open.py
@@ -1,22 +1,15 @@
 from PIL import Image
 import numpy as np
 import cv2
-import gzip
 np.set_printoptions(suppress=True)
 
 
-with gzip.open('image.txt.gz', 'rb') as f:
-    file_content = f.read()
-temp = open("temp.txt", "wb")
-temp.write(file_content)
-temp.close()
-with open("temp.txt", "r") as f:
-    f = f.read()
-    height = int(f[:64], 2)
-    width = int(f[64:128], 2)
-    bitImg = f[128:]
-    parts = [bitImg[i:i+9] for i in range(0, len(bitImg), 9)]
-    bitImg = ''.join(parts)
+with open('flag', 'rb') as f:
+    f = np.frombuffer(f.read(), dtype='int8')
+    # print(f)
+    height = int(f[:1])
+    width = int(f[1:2])
+    bitImg = f[2:]
 
 order = [0, 1, 8, 16, 9, 2, 3, 10, 17, 24, 32, 25, 18, 11, 4, 5, 12, 19, 26, 33, 40, 48, 41, 34, 27, 20, 13, 6, 7, 14, 21, 28, 35, 42, 49, 56, 57, 50, 43, 36, 29, 22, 15, 23, 30, 37, 44, 51, 58, 59, 52, 45, 38, 31, 39, 46, 53, 60, 61, 54, 47, 55, 62, 63]
 
@@ -26,25 +19,22 @@ for i in range(0, height, 8):
     for j in range(0, width, 8):
         block = np.zeros((8, 8))
         for index in range(pointer, pointer + 64):
-            sign = int(parts[index][0])
-            if sign == 0:
-                sign = -1
-            else: sign = 1
             blockI = order[index % 64] // 8
             blockJ = order[index % 64] % 8
-            value = int(parts[index][1:], 2)
-            block[blockI, blockJ] = sign * value
+            value = int(bitImg[index])# [1:], 2)
+            if value < 0:
+                value = abs(value) - 128
+            block[blockI, blockJ] = value # * sign
         pointer += 64
         # print(block)
         reduArr[i:i+8,j:j+8] = block
 
 # defines DCT
 # dctArr = np.zeros((height, width))
-# print(reduArr[0:17,0:17])
+print(reduArr[0:17,0:17])
 
 def idct2(arr):
-    return cv2.idct(arr/255)*255
-    # return fftpack.idct(fftpack.idct(arr, type=3, axis=0, norm='ortho'), type=3, axis=1, norm='ortho')
+    return cv2.idct(arr/128)*128
 
 # thresholds table
 scalar = np.array([16, 11, 10, 16, 24, 40, 51, 61,
@@ -76,6 +66,7 @@ for i in range(0, height, 8):
 for i in range(0, height):
     for j in range(0, width):
         unreduArr[i,j] = unreduArr[i,j].round()
+        unreduArr[i,j] *= 2
 
 # print("After IDCT:")
 # print(unreduArr[160:168,160:168])
