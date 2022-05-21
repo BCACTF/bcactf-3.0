c9c624e26ade0d6aa4a06c27bbb95eef05f8fea0
awt-256 <68395886+awt-256@users.noreply.github.com>
2022-05-21 09:19:30 -0400

    docker binex up to date with main

---
+++ format-fortune/src.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+int magic = 0;
+
+void printflag() {
+    FILE *fptr = fopen("flag.txt", "r");
+    char flag[100];
+    if(fptr == NULL){
+        puts("Come on... Unfortunately we forgot the flag... get it, unfortunately - ha ha ha.\n");
+        puts("Anyways, challenge is misconfigured. Please contact admin.");
+    }
+
+    fgets(flag, sizeof(flag), fptr);
+    puts(flag);
+}
+
+int main() {
+    char buf[50]; 
+
+    setbuf(stdout, NULL);
+    setbuf(stdin, NULL);
+    setbuf(stderr, NULL);
+
+    printf("Welcome to my casino! You look like you want to throw your life savings away...\n");
+    printf("Don't tell anyone, but around here, the house ALWAYS wins.\n");
+    printf("Give it a try! But first, what's your name?\n");
+    fgets(buf, 50, stdin);
+    printf("Well, good luck, ");
+    printf(buf);
+    sleep(1);
+    puts("Testing your luck... please wait");
+    sleep(2);
+    if (magic == 0xBEEF) {
+        puts("\nI'm impressed! You've won a flag");
+        printflag();
+    } else puts("\nI warned you! No flag today.");
+
+    return 1;
+}
