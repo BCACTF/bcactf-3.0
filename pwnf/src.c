a9b30101391a35d533d90deb913dc9955b3082f5
awt-256 <68395886+awt-256@users.noreply.github.com>
2022-05-28 14:14:34 -0400

    pwnf solve and update src to make it solveable(r)

--- pwnf/src.c
+++ pwnf/src.c
@@ -1,14 +1,13 @@
 #include <stdio.h>
 #include <unistd.h>
-#include <strings.h>
+#include <string.h>
 #include <stdlib.h>
 
 int echo() {
-    char input[24];
+    char input[26];
 
-    fgets(input, 24, stdin);
-
-    if (strlen(input) <= 0) return 1;
+    fgets(input, 26, stdin);
+    if (strlen(input) <= 1) return 1;
 
     sleep(1);
     printf(input);
@@ -20,10 +19,11 @@ int main() {
     setbuf(stdout, NULL);
     setbuf(stdin, NULL);
     setbuf(stderr, NULL);
+
     puts("Welcome to my ECHO server! Enjoy.");
 
     while (echo() == 0) {}
-    
+
     puts("Goodbye.");
 
     return 0;
