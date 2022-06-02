f079ecdc53526a458628a285b84619b04ecdea22
kna27 <krisharora27@gmail.com>
2022-06-02 00:10:43 -0400

    Clean up code

--- jasons-tarot-2/server/index.js
+++ jasons-tarot-2/server/index.js
@@ -1,22 +1,20 @@
 const express=require("express");
 const decode=require('jwt-decode');
-const { readFileSync, read }=require("fs");
+const { readFileSync }=require("fs");
 const cookieParser = require("cookie-parser");
-const jwt=require("jsonwebtoken");
+const jwt = require("jsonwebtoken");
 var path = require('path');
-const url=require('url');
 
-const port=1337;
+const port = 1337;
 
-let secret=readFileSync('secret.txt');
-let data={
-	isSubscriber: false,
+let secret = readFileSync('secret.txt');
+let data = {
+	isSubscriber: true,
 };
 const defaultToken = jwt.sign(data, secret);
 const defaultResponse = '{"message":0}';
 const malformedResponse = '{"message":1}';
 const specialResponse = '{"message":"bcactf{hm@c_256_yeeeeah_24u9402}"}';
-//const specialResponse=readFileSync('flag.txt');
 
 var app = express();
 
@@ -26,54 +24,28 @@ app.use(express.static(dir));
 app.use(cookieParser());
 
 app.get('/checktoken', (req, res) => {
-        var retVal = defaultResponse;
-        console.log("starting checktoken");
-        if(req.cookies.token){
-                console.log("found token: "+req.cookies.token);
-                /*if (req.cookies.token == specialToken){
-                        console.log("special token");
-                        retVal=specialResponse;
-                }
-                else {
-                        // failed special token, reset cookie
-                        console.log("not special token");
-                }*/
-		try{
-			if (jwt.verify((req.cookies.token),secret)){
-				let jwt=req.cookies.token;
+	var retVal = defaultResponse;
+	if (req.cookies.token) {
+		try {
+			if (jwt.verify((req.cookies.token), secret)) {
+				let jwt = req.cookies.token;
 				//valid token
-				let decoded=decode(jwt)
-				console.log("decoded token:"+decoded);
-				console.log("decoded token.isSubscriber="+decoded.isSubscriber);
-				console.log("decoded token['isSubscriber']="+decoded['isSubscriber']);
-				//let jwtObj=JSON.parse(decoded);
-				if(decoded.isSubscriber==true){
-					//res.send("flag get");
-					retVal=specialResponse;
-				}
-				else{
-					//res.send("tarot get!");
-					console.log("not subsrcibe~!");
+				let decoded = decode(jwt)
+				if (decoded.isSubscriber == true) {
+					retVal = specialResponse;
 				}
 			}
-		}	
-		catch{
-			console.log("malformed jwt");
-			retVal=malformedResponse;
+		}
+		catch {
+			retVal = malformedResponse;
 		}
 	}
-        else {
-                // no cookie present, set default
-                console.log("No cookie token");
-        }
-	console.log("Resetting token to default and returning "+retVal);
-	console.log("Think i'm resetting token to "+defaultToken);
 
-        res.setHeader('Content-Type', 'application/json');
-        res.cookie('token', defaultToken);
-        res.send(retVal);
+	res.setHeader('Content-Type', 'application/json');
+	res.cookie('token', defaultToken);
+	res.send(retVal);
 });
 
 app.listen(port, () => {
-        console.log("The server is running http://localhost:1337");
+	console.log(`The server is running http://localhost:${port}`);
 });
