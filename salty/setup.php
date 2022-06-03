69ecaeac133b6c910d633d1c422bfc9b55e16d82
zsofiagordon <68395839+zsofiagordon@users.noreply.github.com>
2022-06-02 23:13:12 -0400

    Update setup.php

--- salty/setup.php
+++ salty/setup.php
@@ -1,7 +1,7 @@
 <?php
-//require("../db.php");
+require("../db.php");
   
-//if(!file_exists(DB_FILE)){
+if(!file_exists(DB_FILE)){
        	$user="admin";
         $password="bca4";
         $salt="NaCl";
@@ -13,4 +13,4 @@
         $db->exec("create table users (session_id char(255), user_id char(255), hash char(255), salt char(255))");
 
         $db->exec("insert into users (session_id, user_id, hash, salt) values ('default', '{$user}', '{$hash}', '{$salt}');");
-//}
+}
